13823301f0c18abfbee25e9c54c73f0dd43e0cdf
Daniel Trstenjak <daniel.trstenjak@gmail.com>
2012-07-22 19:07:08 +0200

    Rename apply_ -> applyAE

--- FileData/ApplyToEntity.hs
+++ FileData/ApplyToEntity.hs
@@ -10,14 +10,14 @@ instance Monoid b => ApplyToEntity Entity b where
    apply f entity = f entity
 
 instance Monoid b => ApplyToEntity Layer b where
-   apply f (Layer _ entities _) = apply_ f entities
+   apply f (Layer _ entities _) = applyAE f entities
 
 instance Monoid b => ApplyToEntity Level b where
    apply f (Level _ entities layers) =
-      (apply_ f entities) `mappend` (apply_ f layers)
+      (applyAE f entities) `mappend` (applyAE f layers)
 
 instance Monoid b => ApplyToEntity Data b where
-   apply f (Data _ levels) = apply_ f levels
+   apply f (Data _ levels) = applyAE f levels
 
-apply_ :: (Monoid b, ApplyToEntity a b) => (Entity -> b) -> [a] -> b
-apply_ f apps = mconcat $ map (\app -> apply f app) apps
+applyAE :: (Monoid b, ApplyToEntity a b) => (Entity -> b) -> [a] -> b
+applyAE f apps = mconcat $ map (\app -> apply f app) apps
