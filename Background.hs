a7cee6f679ac5ced2bf266eb4aac89ffac91a0d6
Daniel Trstenjak <daniel.trstenjak@gmail.com>
2012-08-19 15:21:33 +0200

    Add background data structure and functions

---
+++ Background.hs
@@ -0,0 +1,32 @@
+
+module Background where
+import Control.Monad (forM_)
+import qualified Graphics.Rendering.OpenGL.Raw as GL
+import qualified Gamgine.Ressources as R
+import qualified Gamgine.Gfx as G
+import Gamgine.Gfx ((<<<*), (<<*))
+
+
+data Background = Background {
+   size      :: (Double, Double),
+   textureId :: GL.GLuint
+   } deriving Show
+
+
+newBackground :: IO Background
+newBackground = do
+   fp  <- R.getImageFilePath "Background.png"
+   tex <- G.makeTexture2d fp GL.gl_REPEAT
+   return Background {size = (2000, 1000), textureId = tex}
+
+
+render :: Background -> IO ()
+render Background {size = size, textureId = texId} = do
+   G.withTexture2d texId $
+      G.withPrimitive GL.gl_QUADS $ do
+         let coords   = G.quadTexCoords 100 100
+             vertices = G.quad (0,0) size
+         GL.glColor3f <<<* (1,1,1)
+         forM_ (zip coords vertices) (\(c,v) -> do
+            GL.glTexCoord2f <<* c
+            GL.glVertex2f <<* v)
