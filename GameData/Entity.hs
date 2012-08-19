147d93d0269bd59fd9e4562cfabef61c7c2620c2
Daniel Trstenjak <daniel.trstenjak@gmail.com>
2012-08-19 14:15:18 +0200

    Add doc for entity

--- GameData/Entity.hs
+++ GameData/Entity.hs
@@ -5,20 +5,39 @@ import qualified Gamgine.Math.BoxTree as BT
 import qualified FileData.Data2 as FD
 import qualified Event as EV
 
+-- | for classification of a box inside the box tree
 data Position = Top | Bottom | Left | Right | Center | Whatever deriving (Show, Eq)
-type Bound    = BT.BoxTree Position
 
-data Scope = LevelScope | ActiveLayerScope | InactiveLayerScope deriving (Show, Eq)
+-- | the bound of an entity
+type Bound = BT.BoxTree Position
+
+-- | the scope of the entity
+data Scope = LevelScope         -- ^ entity is part of all layers
+           | ActiveLayerScope   -- ^ entity is part of the currently active layer
+           | InactiveLayerScope -- ^ entity is part of a currently inactive layer
+           deriving (Show, Eq)
 
 -- | the type class for all game entities
 class EntityT e where
-   update      :: e -> Scope -> e
+   -- | init ressources of entity e.g. load textures
+   initRessources :: e -> Scope -> IO e
+   initRessources e _ = return e
 
-   render      :: e -> Scope -> IO e
+   -- | update the properties of the entity e.g. it's position
+   update :: e -> Scope -> e
+   update e _ = e
 
+   -- | render the current state of the entity
+   render :: e -> Scope -> IO ()
+   render e _ = return ()
+
+   -- | handle a event send to the entity
    handleEvent :: e -> EV.Event -> Scope -> e
+   handleEvent e _ _ = e
 
-   getBound    :: e -> Maybe Bound
+   -- | returns the bound of the entity, used for collision detection
+   getBound :: e -> Maybe Bound
+   getBound _ = Nothing
 
 
 -- | convert a game data entity into a file data entity
