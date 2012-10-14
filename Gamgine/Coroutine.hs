435484071557fa1ae3c65ef84235f109d004f8c0
Daniel Trstenjak <daniel.trstenjak@gmail.com>
2012-10-14 10:48:48 +0200

    Remove Monad constraint from type definition

--- Gamgine/Coroutine.hs
+++ Gamgine/Coroutine.hs
@@ -1,11 +1,10 @@
 
 module Gamgine.Coroutine where
-import Control.Monad
 
 newtype Coroutine i o = Coroutine {
    runCoroutine :: i -> (o, Coroutine i o)
    }
 
-newtype (Monad m) => CoroutineM m i o = CoroutineM {
+newtype CoroutineM m i o = CoroutineM {
    runCoroutineM :: i -> m (o, CoroutineM m i o)
    }
