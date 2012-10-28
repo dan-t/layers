eccc24b5f44315fea2897e50a02c94eef3328779
Daniel Trstenjak <daniel.trstenjak@gmail.com>
2012-10-28 15:32:06 +0100

    Represent levels/current level as zipper

--- GameData/Data.hs
+++ GameData/Data.hs
@@ -2,38 +2,43 @@
 module GameData.Data where
 #include "Gamgine/Utils.cpp"
 import qualified Data.List as L
+import qualified Data.List.Zipper as LZ
 import qualified GameData.Level as LV
 IMPORT_LENS
 
 
 data Data = Data {
-   finishedLevels  :: [LV.Level],
-   currentLevel     :: LV.Level,
-   followingLevels :: [LV.Level]
+   levels :: LZ.Zipper LV.Level
    } deriving Show
 
-LENS(finishedLevels)
-LENS(currentLevel)
-LENS(followingLevels)
+LENS(levels)
+
+currentLevelL    = currentLevelLens
+currentLevelLens = LE.lens getCurrentLevel setCurrentLevel
+   where
+      getCurrentLevel dat       = LZ.cursor . levels $ dat
+      setCurrentLevel level dat = LE.modL levelsL (LZ.replace level) dat
 
 
 newData :: [LV.Level] -> Data
-newData (curLv : folLvs) = Data [] curLv folLvs
+newData levels = Data $ LZ.fromList levels
 
 
 allLevels :: Data -> [LV.Level]
-allLevels Data {finishedLevels = finLvs, currentLevel = curLv, followingLevels = folLvs} =
-   LV.sortById $ finLvs ++ [curLv] ++ folLvs
+allLevels dat = LV.sortById . LZ.toList . levels $ dat
 
 
 switchToNextLevel :: Data -> Data
-switchToNextLevel d@Data {followingLevels = []} = d
-switchToNextLevel d@Data {finishedLevels = finLvs, currentLevel = curLv, followingLevels = folLvs} =
-   d {finishedLevels  = finLvs ++ [curLv],
-      currentLevel    = L.head folLvs,
-      followingLevels = L.tail folLvs}
+switchToNextLevel d@Data {levels = lvs}
+   | not . LZ.endp $ lvs = d {levels = LZ.right lvs}
+   | otherwise           = d
+
+
+switchToPreviousLevel :: Data -> Data
+switchToPreviousLevel d@Data {levels = lvs}
+   | not . LZ.beginp $ lvs = d {levels = LZ.left lvs}
+   | otherwise             = d
 
 
 atLastLevel :: Data -> Bool
-atLastLevel Data {followingLevels = []} = True
-atLastLevel _                           = False
+atLastLevel dat = LZ.endp . levels $ dat
