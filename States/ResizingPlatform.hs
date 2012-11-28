7a1cafb4a2e5a3d1a0bfe0d0c9fedef8e50054f4
Daniel Trstenjak <daniel.trstenjak@gmail.com>
2012-11-28 19:33:24 +0100

    Fix resizing of platforms

--- States/ResizingPlatform.hs
+++ States/ResizingPlatform.hs
@@ -15,29 +15,37 @@ import qualified Entity.Id as EI
 import qualified Entity.Position as EP
 import qualified States.State as ST
 import qualified States.GameRunning as GR
+import qualified States.CreatingPlatform as CP
 IMPORT_LENS
 
 
 data ResizingPlatform = ResizingPlatform {
    entityId :: Maybe Int,
-   startPos :: V.Vect,
+   minPt    :: V.Vect,
+   maxPt    :: V.Vect,
    basePos  :: V.Vect
    }
 
 -- | the state for resizing a platform during edit mode
 mkResizingPlatformState :: ST.State GD.Data
 mkResizingPlatformState =
-   mkState $ ResizingPlatform Nothing V.nullVec V.nullVec
+   mkState $ ResizingPlatform Nothing V.nullVec V.nullVec V.nullVec
    where
       mkState rp = ST.State {
          ST.enter = \mp gd ->
             case LV.findEntityAt mp $ LE.getL GD.currentLevelL gd of
-                 Just e@E.Platform {} -> Just (gd, mkState (rp {entityId = Just $ EI.entityId e,
-                                                                startPos = mp,
-                                                                basePos  = B.maxPt . E.platformBound $ e}))
-                 _                    -> Nothing,
+                 Just e@E.Platform {} ->
+                    let pos   = EP.position e
+                        bound = E.platformBound e
+                        minPt = pos + B.minPt bound
+                        maxPt = pos + B.maxPt bound
+                        in Just (gd, mkState (rp {entityId = Just $ EI.entityId e,
+                                                  minPt    = minPt,
+                                                  maxPt    = maxPt,
+                                                  basePos  = mp}))
+                 _ -> Nothing,
 
-         ST.leave      = (, mkState (rp {entityId = Nothing, startPos = V.nullVec, basePos = V.nullVec})),
+         ST.leave      = (, mkState (rp {entityId = Nothing, minPt = V.nullVec, maxPt = V.nullVec, basePos = V.nullVec})),
          ST.update     = (, mkState rp) . GR.update,
          ST.render     = ((, mkState rp) <$>) .: GR.render,
          ST.keyEvent   = (, mkState rp) .: flip const,
@@ -45,9 +53,9 @@ mkResizingPlatformState =
 
          ST.mouseMoved = \mp gd ->
             case rp of
-                 ResizingPlatform {entityId = Just id, startPos = sp, basePos = bp} ->
-                    let diffVec = mp - sp
-                        in (E.eMap (\e -> id == EI.entityId e ? LE.modL E.platformBoundL (\pb -> pb {B.maxPt = bp + diffVec}) e $ e) gd,
+                 ResizingPlatform {entityId = Just id, minPt = minPt, maxPt = maxPt, basePos = bp} ->
+                    let diffVec = mp - bp
+                        in (E.eMap (\e -> id == EI.entityId e ? CP.updatePosAndBound minPt (maxPt + diffVec) e $ e) gd,
                             mkState rp)
 
                  _ -> (gd, mkState rp)
