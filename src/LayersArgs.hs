e0b27623d67a96df92d5a4df14caf68bb5c18be4
Daniel Trstenjak <daniel.trstenjak@gmail.com>
2014-01-04 00:48:16 +0100

    layers 0.2.3

--- src/LayersArgs.hs
+++ src/LayersArgs.hs
@@ -57,7 +57,7 @@ detailsText = [
 defaultSaveTo = "LayersData.hs"
 defaultLoadFrom = unsafePerformIO $ RES.getDataFileName "Ressources/Levels.hs"
 
-versionInfo = "layers version 0.2.2"
+versionInfo = "layers version 0.2.3"
 summaryInfo = ""
 
 getLayersArgs :: IO LayersArgs
